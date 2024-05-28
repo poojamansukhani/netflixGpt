3f72d958854952f20e788a64d1746cb422c36f15
Pooja Mansukhani <poojamansukhani94@gmail.com>
2024-05-28 18:42:00 +0530

    handle logic updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom"
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Body = () => {
     const appRouter = createBrowserRouter([
@@ -14,6 +16,19 @@ const Body = () => {
             element:<Browse/>
         }
     ])
+    const auth = getAuth();
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              const {uid, email, displayNanme} = user;
+              
+              // ...
+            } else {
+              // User is signed out
+              // ...
+            }
+          });
+    },[])
   return (
     <>
         <RouterProvider router={appRouter}/>
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,7 @@
-export const validate = (email, password, name) => {
+export const validate = (email, password) => {
     const isEmailValid = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email);
-    const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
-    const isNameValid = /[a-zA-Z]+\\.?/.test(name);
+    const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,}$/.test(password);
+    //const isNameValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
     // if(!isNameValid){
     //     return "Name is not valid";
     // }
